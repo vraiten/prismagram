7a95d6ac9c0d8c8c44479a18097798d084909869
jp798 <cac.jpkim@gmail.com>
2019-06-01 01:13:23 +0900

    upload

---
+++ src/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+    type Query{
+        hello: String!
+    }
+`;
+
+const resolvers = {
+
+    Query: {
+
+        hello: () => "HI"
+    }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.express.use(logger("dev"));
+
+
+server.start({ port: PORT }, () =>
+    console.log(`Server running on port on http://localhost:${PORT}`)
+);
