0b9366b62513f12097d6671c94da32d83ea7eea6
jp798 <cac.jpkim@gmail.com>
2019-06-05 19:40:02 +0900

    upload

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,43 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({path: path.resolve(__dirname,".env") });
+
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+
+
 import { adjectives,nouns} from "./words";
 
 export const generateSecret = () => {
 
 	const randomNumber = Math.floor(Math.random() * adjectives.length);
 	return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+console.log(process.env.SENDGRID_PASSWORD, process.env.SENDGRID_USERNAME);
+
+const sendMail = email =>{
+
+	const options = {
+		auth: {
+			api_user: process.env.SENDGRID_USERNAME,
+			api_key: process.env.SENDGRID_PASSWORD
+		}
+	};
+
+	const client = nodemailer.createTransport(sgTransport(options));
+	return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+ const email = {
+
+	from : "jp798@naver.com",
+	to: address,
+	subject: "Login Secret for Prismagram.. ",
+	html : `Hello! Your login secret it ${secret}.<br/> Copy paste on the app/website to log in. `
+
+ };
+ return sendMail(email);
+
 };
